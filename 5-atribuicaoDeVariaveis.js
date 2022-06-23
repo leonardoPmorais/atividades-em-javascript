3ea8c4bddf2a1225aa67378e3bea7fc848d84957
Leonardo pereira de Morais <90428256+leonardoPmorais@users.noreply.github.com>
2022-06-23 01:47:19 -0300

    adicionando projetos em javascript

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Atribuições de Variáveis");
+
+
+let primeiroNome = "Leonardo";
+let sobrenome = "Morais";
+
+//console.log(nome + sobrenome);
+//console.log(nome, sobrenome);
+console.log(primeiroNome + " " + sobrenome);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade;
+idade = 26;
+idade = idade +1;
+console.log(idade);
+
+
+
